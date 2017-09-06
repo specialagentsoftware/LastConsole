d9415a6c6e156ab50fc9529584ab07d848df10ad
Jeremy Bond <bond_jeremy_w@outlook.com>
2017-09-06 15:35:46 -0400

    first file push

---
+++ ConsoleApp1/ConsoleApp1/Example.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Threading;
+using HidLibrary;
+using ScaleInterface;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ScaleReader
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      decimal? weight;
+      bool? isStable;
+
+      USBScale s = new USBScale();
+      s.Connect();
+
+      if (s.IsConnected)
+      {
+        s.GetWeight(out weight, out isStable);
+        s.DebugScaleData();
+        s.Disconnect();
+        Console.WriteLine("Weight: {0:0.00} LBS", weight);
+        Console.ReadLine();
+            } else {
+        Console.WriteLine("No Scale Connected.");
+        Console.ReadLine();
+            }
+    }
+  }
+}
